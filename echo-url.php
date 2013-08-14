d7fbf9a601569d8d327e8e6543084162f7ad0a4b
Tomaz Trskan <tomaz.trskan@tristo.si>
2013-08-14 10:27:43 +0200

    Require menu and some little styling

--- echo-url.php
+++ echo-url.php
@@ -1,3 +1,3 @@
 <?php
-include 'menu.php';
-echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+require 'menu.php';
+echo 'The URL is: <strong>http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'</strong><br />';
