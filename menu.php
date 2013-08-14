4e6656cba888e8685b9604f40c4d900dd000d465
Tomaz Trskan <tomaz.trskan@tristo.si>
2013-08-14 10:21:03 +0200

    Created menu for this app

---
+++ menu.php
@@ -0,0 +1,8 @@
+<?php
+
+echo 'This is the menu of this application: ';
+
+echo '<a href="/" title="Home">Home</a> | ';
+echo '<a href="" title="Display name">Display name</a> | ';
+echo '<a href="" title="Echo URL">Echo URL</a>';
+echo '<hr />';
